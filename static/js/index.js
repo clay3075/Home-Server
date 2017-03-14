4d3d3cfb48a26ebd25dd2d0b6ed6bddebe920e07
Clay Reddick <cjreddick96@gmail.com>
2017-03-13 20:05:19 -0500

    Simple home server using python flask.

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#music_icon').click(function() {
+        console.log("music click");
+    });
+    $('#video_icon').click(function() {
+        console.log("video click");
+    });
+});
+
