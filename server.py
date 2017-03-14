cc1967e62b6e14c1aba44c65b12494831d7cf678
Clay Reddick <cjreddick96@gmail.com>
2017-03-13 21:05:37 -0500

    Added basic outline for video and music pages.

--- server.py
+++ server.py
@@ -2,8 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html") 
+
+@app.route("/music")
+def music():
+    return render_template("music.html");
+
+@app.route("/video")
+def video():
+    return render_template("video.html");
 
 if __name__ == "__main__":
     app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $('#music_icon').click(function() {
         console.log("music click");
+		window.location = "music";
     });
     $('#video_icon').click(function() {
         console.log("video click");
+		window.location = "video";
     });
 });
 
